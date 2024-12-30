b6ec79b913c1614acb9573dafafbf868196b505b
Akmal Najib <135620988+BlazCat@users.noreply.github.com>
2024-12-30 12:40:36 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+    //menampilkan dialog
+    alert ("hello");
+
+    //tipe data string
+    var boxsaya ="sepatu";
+
+    //tipe data number
+    var jumlahbox =5;
+
+    //tipe data bolean
+    var wallet =10000;
+    var hutang =2000;
+
+    console.log(wallet==hutang)
+
+    //menampilkan hasil data
+    console.log(boxsaya)
+    console.log(jumlahbox)
